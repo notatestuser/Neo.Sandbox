f9b2fa10baf2c94df35204b945e7b5d7b1bff0cd
Darcy Thomas <dthomas@specnotes.com>
2017-07-31 20:37:18 -0400

    add utility class with methods to compile and run a smart contract

---
+++ src/Testbox/Utilities/CompileAndRun.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using Neo.Compiler.MSIL;
+using Neo.Cryptography;
+using Neo.VM;
+
+namespace Testbox.Utilities
+{
+    public static class CompileAndRun
+    {
+        private const string SmartContractDirectory = "../../../../";
+
+        public static byte[] Compile(string projectName)
+        {
+            return Converter.Convert(new FileStream(Path.Combine(SmartContractDirectory, projectName, @"bin\Debug", $"{projectName}.dll"), FileMode.Open));
+        }
+
+        public static void Run(byte[] program)
+        {
+            ExecutionEngine ee = new ExecutionEngine(null, Crypto.Default, null, null);
+            ee.LoadScript(program);
+            ee.Execute();
+        }
+    }
+}

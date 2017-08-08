ba99d14158822e58a086b7ff032bf35b1c5f4ab2
Darcy Thomas <dthomas@specnotes.com>
2017-08-08 00:12:01 -0400

    *add simple example for passing in arguments

---
+++ src/Calculator/Calculator.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Neo.SmartContract.Framework;
+
+namespace Calculator
+{
+    public class Calculator : FunctionCode
+    {
+        public static int Main(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}

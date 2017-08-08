a70649df5a6c14cba9cb7fb3ffaae60955ad93e2
Darcy Thomas <dthomas@specnotes.com>
2017-08-08 00:10:52 -0400

    *refactor class name

--- src/Testbox/Utilities/ExecutionHelper.cs
+++ src/Testbox/Utilities/ExecutionHelper.cs
@@ -3,12 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using Neo.Compiler.MSIL;
-using Neo.Cryptography;
 using Neo.VM;
 
 namespace Testbox.Utilities
 {
-    public static class CompileAndRun
+    public static class ExecutionHelper
     {
 #if DEBUG 
         private const string Directory = "Debug";
@@ -21,13 +20,6 @@ namespace Testbox.Utilities
         public static byte[] Compile(string projectName)
         {
             return Converter.Convert(new FileStream(Path.Combine(SmartContractDirectory, projectName, $@"bin\{Directory}", $"{projectName}.dll"), FileMode.Open));
-        }
-
-        public static void Run(byte[] program)
-        {
-            ExecutionEngine ee = new ExecutionEngine(null, Crypto.Default, null, null);
-            ee.LoadScript(program);
-            ee.Execute();
         }
     }
 }

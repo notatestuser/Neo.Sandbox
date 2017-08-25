a6ef18adee7ab80c2d1804e4a688bf096513f140
Aljoscha Krettek <aljoscha.krettek@gmail.com>
2017-08-25 08:37:08 +0200

    Make build files work on macOS

--- src/Testbox/Utilities/ExecutionHelper.cs
+++ src/Testbox/Utilities/ExecutionHelper.cs
@@ -10,16 +10,16 @@ namespace Testbox.Utilities
     public static class ExecutionHelper
     {
 #if DEBUG 
-        private const string Directory = "Debug";
+        private const string Directory = "Debug/netstandard1.6";
 #else
-        private const string Directory = "Release";
+        private const string Directory = "Release/netstandard1.6";
 #endif
    
         private const string SmartContractDirectory = "../../../../";
 
         public static byte[] Compile(string projectName)
         {
-            return Converter.Convert(new FileStream(Path.Combine(SmartContractDirectory, projectName, $@"bin\{Directory}", $"{projectName}.dll"), FileMode.Open));
+            return Converter.Convert(new FileStream(Path.Combine(SmartContractDirectory, projectName, $@"bin/{Directory}", $"{projectName}.dll"), FileMode.Open));
         }
     }
 }
